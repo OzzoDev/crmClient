9dab683bc1db2f6fc8958922157b983db45f67a0
oscar <oscar.burman005@gmail.com>
2024-11-10 20:14:57 +0100

    Hooked up all input to own eventlistener to deal with falsy inputs

--- script.js
+++ script.js
@@ -1,77 +1,130 @@
 const userForm = document.getElementById("userForm");
 const userEmail = document.getElementById("userEmail");
+const confirmUserEmail = document.getElementById("confirmUserEmail");
+const userPassword = document.getElementById("userPassword");
+const confirmUserPassword = document.getElementById("confirmUserPassword");
+
+const userEmailLabel = document.getElementById("userEmailLabel");
+const confirmUserEmailLabel = document.getElementById("confirmUserEmailLabel");
+const userPasswordLabel = document.getElementById("userPasswordLabel");
+
+const confirmUserPasswordLabel = document.getElementById("confirmUserPasswordLabel");
+
 const submitMessage = document.getElementById("submitMessage");
 const usersList = document.getElementById("users");
 
-userForm.addEventListener("submit",(e)=>{
-    e.preventDefault();
+userForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const email = userEmail.value.trim().toLocaleLowerCase();
+  const email = userEmail.value.trim().toLocaleLowerCase();
+  const confimEmail = confirmUserEmail.value.trim().toLocaleLowerCase();
+  const password = userPassword.value.trim().toLocaleLowerCase();
+  const confimPassword = confirmUserPassword.value.trim().toLocaleLowerCase();
 
-    if(email===""){
-        submitMessage.innerText="Field must not be empty";
-        submitMessage.classList.add("error")
-        submitMessage.classList.remove("success")
-    }else{
-        submitMessage.innerText="User created";
-        submitMessage.classList.add("success")
-        submitMessage.classList.remove("error");
+  if (email === "") {
+    submitMessage.innerText = "Field must not be empty";
+    submitMessage.classList.add("error");
+    submitMessage.classList.remove("success");
+  } else {
+    submitMessage.innerText = "User created";
+    submitMessage.classList.add("success");
+    submitMessage.classList.remove("error");
 
-        addUser(email);
+    addUser(email);
+  }
+
+  resetForm();
+});
+
+userEmail.addEventListener("blur", () => {
+  const email = userEmail.value.trim().toLocaleLowerCase();
+  if (email === "") {
+    userEmailLabel.innerText = "Fältet får inte vara tomt";
+  } else if (!email.includes("@") || !email.includes(".")) {
+    userEmailLabel.innerText = "Ange en giltig mejladress";
+  } else {
+    userEmailLabel.innerText = "";
+  }
+});
+
+confirmUserEmail.addEventListener("blur", () => {
+  const confirmEmail = confirmUserEmail.value.trim().toLocaleLowerCase();
+  const email = userEmail.value.trim().toLocaleLowerCase();
+
+  if (email !== confirmEmail) {
+    confirmUserEmailLabel.innerText = "Mejladresserna måste vara samma";
+  } else {
+    confirmUserEmailLabel.innerText = "";
+  }
+});
+
+userPassword.addEventListener("blur", () => {
+  const password = userPassword.value.trim().toLocaleLowerCase();
+  if (password.length < 8) {
+    userPasswordLabel.innerText = "Lösenordet måste vara minst 8 tecken långt";
+  } else {
+    userPasswordLabel.innerText = "";
+  }
+});
+
+confirmUserPassword.addEventListener("blur", () => {
+  const confimPassword = confirmUserPassword.value.trim().toLocaleLowerCase();
+  const password = userPassword.value.trim().toLocaleLowerCase();
+
+  if (password !== confimPassword) {
+    confirmUserPasswordLabel.innerText = "Lösenorden måste vara samma";
+  } else {
+    confirmUserEmailLabel.innerText = "";
+  }
+});
+
+async function getUsers() {
+  try {
+    const res = await fetch("http://localhost:3000/users");
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    } else {
+      const users = await res.json();
+      console.log("Users: ", users);
     }
-
-    resetForm();
-})
-
-async function getUsers(){
-    try{
-        const res = await fetch("http://localhost:3000/users");
-        if(!res.ok){
-            throw new Error("Network response was not ok")
-        }else{
-            const users = await res.json();
-            console.log("Users: ",users)
-        }
-    }catch(error){
-        console.log("There was a problem with fetching users");
-    }
+  } catch (error) {
+    console.log("There was a problem with fetching users");
+  }
 }
 
-async function addUser(user){
-    try{
-        const res = await fetch("http://localhost:3000/users",{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({user:user})
-        });
-        if(!res.ok){
-            throw new Error("Network response was not ok")
-        }else{
-            const users = await res.json();
-            console.log("Users: ",users)
-            renderUser(users);
-        }
-
-    }catch(error){
-        console.log(`There was a problem adding user ${user}`);
-    }
-}
-
-function renderUser(users){
-
-    usersList.innerHTML="";
-    const usersAsArray = users.users;
-
-    usersAsArray.forEach(user => {
-        const li = document.createElement("li");
-        li.innerText=user
-        console.log("li",li)
-        usersList.appendChild(li);
+async function addUser(user) {
+  try {
+    const res = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user: user }),
     });
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    } else {
+      const users = await res.json();
+      console.log("Users: ", users);
+      renderUser(users);
+    }
+  } catch (error) {
+    console.log(`There was a problem adding user ${user}`);
+  }
 }
 
-function resetForm(){
-    userEmail.value="";
+function renderUser(users) {
+  usersList.innerHTML = "";
+  const usersAsArray = users.users;
+
+  usersAsArray.forEach((user) => {
+    const li = document.createElement("li");
+    li.innerText = user;
+    console.log("li", li);
+    usersList.appendChild(li);
+  });
+}
+
+function resetForm() {
+  userEmail.value = "";
 }
